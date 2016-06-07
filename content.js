20ab944c6144ef0e6291003d1747ad0e9ae146c6
Mike Tigas <mike@tig.as>
2016-06-07 19:00:58 -0400

    work-in-progress: troncX, our improved trumping and troncing engine

--- content.js
+++ content.js
@@ -1,5 +1,38 @@
+function getStyle(el,styleProp)
+{
+    var x = document.getElementById(el);
+    if (!x)
+      return null;
+    if (x.currentStyle)
+        var y = x.currentStyle[styleProp];
+    else if (window.getComputedStyle)
+        var y = document.defaultView.getComputedStyle(x,null).getPropertyValue(styleProp);
+    return y;
+}
+function calculateLineHeight (element) {
+
+  var lineHeight = parseInt(getStyle(element, 'line-height'), 10);
+  var clone;
+  var singleLineHeight;
+  var doubleLineHeight;
+
+  if (isNaN(lineHeight)) {
+    clone = element.cloneNode();
+    clone.innerHTML = '<br>';
+    element.appendChild(clone);
+    singleLineHeight = clone.offsetHeight;
+    clone.innerHTML = '<br><br>';
+    doubleLineHeight = clone.offsetHeight;
+    element.removeChild(clone);
+    lineHeight = doubleLineHeight - singleLineHeight;
+  }
+
+  return lineHeight;
+}
 
 function troncIt() {
+    var fancyChance = 1/12;
+
     var elements = document.getElementsByTagName('*');
 
     for (var i = 0; i < elements.length; i++) {
@@ -10,11 +43,27 @@ function troncIt() {
 
             if (node.nodeType === 3) {
                 var text = node.nodeValue;
-                var replacedText = text.replace(/(mr\. )?(trump|donald.*trump)/gi, 'Tronc');
+                if (Math.random() <= fancyChance) {
+                    var eleHeight = calculateLineHeight(element)*17/23;
+                    /*var fancy = document.createElement('img');
+                    fancy.setAttribute("style", eleHeight+'px');
+                    fancy.setAttribute('src', "http://www.tronc.com/tronc-logo.png");
+                    */
+                    var newText = text.replace(
+                      /(mr\. )?(trump|donald.*trump)/gi,
+                      //fancy
+                      '<img style="height:'+eleHeight+'px" src="http://www.tronc.com/tronc-logo.png"/>'
+                    );
+                    var newNode = jQuery("<span>"+newText+"</span>").get(0);
+                    node.parentNode.replaceChild(newNode, node);
 
-                if (replacedText !== text) {
-                    element.replaceChild(document.createTextNode(replacedText), node);
+                } else {
+                    var replacedText = text.replace(/(mr\. )?(trump|donald.*trump)/gi, 'Tronc');
+                    if (replacedText !== text) {
+                        element.replaceChild(document.createTextNode(replacedText), node);
+                    }
                 }
+
             }
         }
     }
@@ -24,4 +73,4 @@ function troncIt() {
 troncIt();
 
 // AND KEEP ON DOING IT!
-setInterval(troncIt, 1000);
+//setInterval(troncIt, 1000);
